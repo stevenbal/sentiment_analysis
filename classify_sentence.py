55068a21867d70f188d9668b41d9e83bbe6ace2a
Steven Bal <steevbal@hotmail.com>
2018-10-11 21:59:06 +0200

    added comments for functions of n-gram model file

--- classify_sentence.py
+++ classify_sentence.py
@@ -13,23 +13,20 @@ else:
 # LM_pos.save_models('models/positive_char_n6.p')
 # LM_neg.save_models('models/negative_char_n6.p')
 
-'''
-Load the ngram models for negative and positive sentiments, n=3 and stopwords
-are omitted
-'''
+
+# Load the ngram models for negative and positive sentiments, n=3 and stopwords
+# are omitted
+
 LM_pos = ngram.LanguageModel(model_file='models/positive_n3_nostopwords.p')
 LM_neg = ngram.LanguageModel(model_file='models/negative_n3_nostopwords.p')
 
 #LM_pos_char = ngram.LanguageModel(model_file='models/positive_char_n6_nostopwords.p')
 #LM_neg_char = ngram.LanguageModel(model_file='models/negative_char_n6_nostopwords.p')
 
-'''
-Construct classifier from the two models
-'''
+# Construct classifier from the two models
 classifier = ngram.Classifier(('positive', LM_pos), ('negative', LM_neg))
 #classifier_char = ngram.Classifier(('positive', LM_pos_char), ('negative', LM_neg_char))
 
-'''
-Determine the sentiment of the given sentence, only use bigram model
-'''
+
+# Determine the sentiment of the given sentence, only use bigram model
 print(classifier.classify(sentence, mixture=[2]))
--- evaluate_corpora.py
+++ evaluate_corpora.py
@@ -8,25 +8,20 @@ from time import time
 # LM_pos.save_models('models/positive_char_n6.p')
 # LM_neg.save_models('models/negative_char_n6.p')
 
-'''
-Load the ngram models for negative and positive sentiments, n=3 and stopwords
-are omitted
-'''
+# Load the ngram models for negative and positive sentiments, n=3 and stopwords
+# are omitted
 LM_pos = ngram.LanguageModel(model_file='models/positive_n3_nostopwords.p')
 LM_neg = ngram.LanguageModel(model_file='models/negative_n3_nostopwords.p')
 
 #LM_pos_char = ngram.LanguageModel(model_file='models/positive_char_n6_nostopwords.p')
 #LM_neg_char = ngram.LanguageModel(model_file='models/negative_char_n6_nostopwords.p')
 
-'''
-Construct classifier from the two models
-'''
+# Construct classifier from the two models
 classifier = ngram.Classifier(('positive', LM_pos), ('negative', LM_neg))
 #classifier_char = ngram.Classifier(('positive', LM_pos_char), ('negative', LM_neg_char))
 
 
-'''
-Evaluate the classifier on the test set and report precision and recall,
-use mixture model of bigram and unigram model
-'''
+
+# Evaluate the classifier on the test set and report precision and recall,
+# use mixture model of bigram and unigram model
 print(classifier.evaluate('corpora/merged_sentiment_data.csv', mixture=[1,2]))
