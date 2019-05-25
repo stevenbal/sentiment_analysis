5b02c9dc70910811201936cf3a117469d0e39b51
Steven Bal <steevbal@hotmail.com>
2019-05-25 11:32:36 +0200

    removed old tests

--- tests/test_NaiveBayesClassifier.py
+++
@@ -1,6 +0,0 @@
-from resources.NaiveBayesClassifier import NaiveBayesClassifier
-import unittest
-
-# import sys
-# sys.path.append(
-#     '/home/steven/Documents/Projects/Python/NLP/sentiment_analysis/')
